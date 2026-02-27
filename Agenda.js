9430234b9e76a204256e50ceaa8995c26c36b007
ErickRochaNascimento <erickrochaluciano@gmail.com>
2026-02-27 10:01:06 -0300

    Agenda

--- Agenda.js
+++ Agenda.js
@@ -8,14 +8,14 @@ export class Agenda {
     // Método: Adicionar novo contato 
     adicionar(contato) {
         this.contatos.push(contato);
-        console.log('[OK] Contato ${contato.nome} adicionado.');
+        console.log(`[OK] Contato ${contato.nome} adicionado.`);
     }
 
     // Método: Listar todos 
     listar() {
         console.log("\n--- MEUS CONTATOS ---");
         this.contatos.forEach(c => {
-            console.log('Nome: ${c.nome} | Tel: ${c.telefone}');
+            console.log(`Nome: ${c.nome} | Tel: ${c.telefone}`);
         });
 
     }
