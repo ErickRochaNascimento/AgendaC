b17cd538279f30cecafd4a3ccb9d0336beb66849
ErickRochaNascimento <erickrochaluciano@gmail.com>
2026-02-27 09:56:44 -0300

    adiciona classe Agenda com métodos para adicionar, listar e buscar contatos

---
+++ Agenda.js
@@ -0,0 +1,26 @@
+export class Agenda {
+    constructor() {
+        this.contatos = [];
+
+    }
+
+
+    // Método: Adicionar novo contato 
+    adicionar(contato) {
+        this.contatos.push(contato);
+        console.log('[OK] Contato ${contato.nome} adicionado.');
+    }
+
+    // Método: Listar todos 
+    listar() {
+        console.log("\n--- MEUS CONTATOS ---");
+        this.contatos.forEach(c => {
+            console.log('Nome: ${c.nome} | Tel: ${c.telefone}');
+        });
+
+    }
+    // Método: Buscar por nome 
+    buscar(nome) {
+        return this.contatos.find(c => c.nome.toLowerCase() === nome.toLowerCase());
+    }
+} 
