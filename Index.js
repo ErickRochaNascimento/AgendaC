23565e49a758f17741ef00512d4e2d28bd39e612
ErickRochaNascimento <erickrochaluciano@gmail.com>
2026-02-27 10:02:09 -0300

    refatora o arquivo Index.js para instanciar objetos da classe Contato e utilizar métodos da classe Agenda

--- Index.js
+++ Index.js
@@ -1,7 +1,13 @@
-export class Contato {
-    constructor(nome, telefone, email) {
-        this.nome = nome;
-        this.telefone = telefone;
-        this.email = email;
-    }
-} 
+import { Contato } from './Contato.js'; 
+import { Agenda } from './Agenda.js';
+const minhaAgenda = new Agenda();
+
+// Instanciando objetos 
+const c1 = new Contato("Helena", "71 98888-0000", "helena@email.com"); 
+const c2 = new Contato("João", "11 97777-1111", "joao@email.com");
+// Usando os métodos    
+minhaAgenda.adicionar(c1); 
+minhaAgenda.adicionar(c2); 
+minhaAgenda.listar(); 
+
+
